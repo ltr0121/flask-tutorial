906895ddfc054f7bfbf35ba21edb98d4fadef4d2
ltr0121 <ttrrr121@gmail.com>
2019-03-20 18:37:31 +0900

    Create a Blueprint (Blueprints and Views)

---
+++ flaskr/auth.py
@@ -0,0 +1,10 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth') # auth라는 blueprint 생성, 정의 위치 알려 줌, 덧붙여지는 url
